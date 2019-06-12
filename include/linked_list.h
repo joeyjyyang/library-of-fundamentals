66162f293071a26760a838202d1e417baa39b72e
Joey Yang <yangj30@mcmaster.ca>
2019-06-11 23:35:22 -0400

    templatized linked list

--- include/linked_list.h
+++ include/linked_list.h
@@ -12,9 +12,12 @@
 #include <iostream>
 #include <fstream>
 #include <sstream>
+#include <string>
+#include <cstdlib>
 
 namespace data_structure
 {
+template <class T>
 class LinkedList 
 {
 public:
@@ -29,10 +32,11 @@ public:
 	/* class methods */
 	bool isEmpty() const;
 	int getSize() const;
-	void appendNode(const int id);
-	void prependNode(const int id);
-	void insertNode(const int id, int position);
-	void removeNode(const int id);
+	T getNodeID(const int position) const;
+	void appendNode(const T id);
+	void prependNode(const T id);
+	void insertNode(const T id, const int position);
+	void removeNode(const T id);
 	void printLinkedList() const;
 	void loadNodes(std::ifstream& nodes_infile);
 	//recursively reverse the Linked List
@@ -42,13 +46,15 @@ public:
 private:
 	struct Node
 	{
-		const int id_;
+		const T id_;
 		Node* next_;
+		
 		Node() : 
 			id_(0), next_(nullptr) 
 		{
 		}
-		Node(const int id) : 
+
+		Node(const T id) : 
 			id_(id), next_(nullptr) 
 		{
 		}
