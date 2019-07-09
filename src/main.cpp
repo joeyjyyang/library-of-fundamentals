35c329a5457311e5f1061472b9079a855fd507e5
Joey Yang <yangj30@mcmaster.ca>
2019-07-08 21:39:05 -0400

    unit testing

--- src/main.cpp
+++ src/main.cpp
@@ -6,77 +6,84 @@
  
 int main(int argc, char *argv[])
 {
-/*
-	data_structure::LinkedList<char> linked_list;
+	std::cout << "------Test Case #1------" << std::endl;
+	{
+		data_structure::LinkedList<int> linked_list;
 	
-	linked_list.appendNode(3);
-	linked_list.appendNode(4);
-	linked_list.printLinkedList();
+		linked_list.appendNode(3);
+		linked_list.appendNode(4);
+		linked_list.printLinkedList();
+		std::cout << "--------------" << std::endl;
 
-	linked_list.prependNode(1);
-	linked_list.printLinkedList();
+		linked_list.prependNode(1);
+		linked_list.printLinkedList();
+		std::cout << "--------------" << std::endl;
 
-	linked_list.insertNode(2, 1);
-	linked_list.printLinkedList();
+		linked_list.insertNode(2, 1);
+		linked_list.printLinkedList();
+		std::cout << "--------------" << std::endl;
 
-	linked_list.insertNode(6, 2);
-	linked_list.printLinkedList();
+		linked_list.insertNode(6, 2);
+		linked_list.printLinkedList();
 
-	linked_list.removeNode(6);
-	linked_list.printLinkedList();
+		linked_list.removeNode(6);
+		linked_list.printLinkedList();
+		std::cout << "--------------" << std::endl;
 
-	linked_list.reverseLinkedList();
-	std::cout << "Reversing Linked List." << std::endl;
-	linked_list.printLinkedList();
-*/
-	data_structure::LinkedList<char> linked_list;
+		linked_list.reverseLinkedList();
+		std::cout << "Reversing Linked List." << std::endl;
+		linked_list.printLinkedList();
+		std::cout << "--------------" << std::endl;
 	
-	linked_list.appendNode('c');
-	linked_list.appendNode('d');
-	linked_list.printLinkedList();
-	std::cout << "--------------" << std::endl;
+		linked_list.getNodeID(0);
+		linked_list.getNodeID(1);
+		linked_list.getNodeID(2);
+		linked_list.getNodeID(3);
+		linked_list.getNodeID(4);
+		std::cout << "--------------" << std::endl;
+	}
+	std::cout << "------Test Case #2------" << std::endl;
+	{
+		data_structure::LinkedList<char> linked_list;
+		
+		linked_list.appendNode('c');
+		linked_list.appendNode('d');
+		linked_list.printLinkedList();
+		std::cout << "--------------" << std::endl;
 
-	linked_list.prependNode('a');
-	linked_list.printLinkedList();
-	std::cout << "--------------" << std::endl;
+		linked_list.prependNode('a');
+		linked_list.printLinkedList();
+		std::cout << "--------------" << std::endl;
 
-	linked_list.insertNode('b', 1);
-	linked_list.printLinkedList();
-	std::cout << "--------------" << std::endl;
+		linked_list.insertNode('b', 1);
+		linked_list.printLinkedList();
+		std::cout << "--------------" << std::endl;
 
-	linked_list.insertNode('f', 2);
-	linked_list.printLinkedList();
-	std::cout << "--------------" << std::endl;
+		linked_list.insertNode('f', 2);
+		linked_list.printLinkedList();
+		std::cout << "--------------" << std::endl;
 
-	linked_list.removeNode('f');
-	linked_list.printLinkedList();
-	std::cout << "--------------" << std::endl;
+		linked_list.removeNode('f');
+		linked_list.printLinkedList();
+		std::cout << "--------------" << std::endl;
 
-	linked_list.reverseLinkedList();
-	std::cout << "Reversing Linked List." << std::endl;
-	linked_list.printLinkedList();
-	std::cout << "--------------" << std::endl;
+		linked_list.reverseLinkedList();
+		std::cout << "Reversing Linked List." << std::endl;
+		linked_list.printLinkedList();
+		std::cout << "--------------" << std::endl;
 
-	linked_list.getNodeID(0);
-	linked_list.getNodeID(1);
-	linked_list.getNodeID(2);
-	linked_list.getNodeID(3);
-	linked_list.getNodeID(4);
-	std::cout << "--------------" << std::endl;
+		data_structure::LinkedList<char> dummy_list1(linked_list);
+		dummy_list1.removeNode('c');
+		dummy_list1.printLinkedList();
+		std::cout << "--------------" << std::endl;
 
-	data_structure::LinkedList<char> dummy_list1(linked_list);
-	dummy_list1.printLinkedList();
-	std::cout << "--------------" << std::endl;
-
-	data_structure::LinkedList<char> dummy_list2;
-	dummy_list2.printLinkedList();
-	linked_list.printLinkedList();
-	std::cout << "--------------" << std::endl;
-
-	dummy_list2 = linked_list;
-	dummy_list2.printLinkedList();
-	linked_list.printLinkedList();
-	std::cout << "--------------" << std::endl;
+		data_structure::LinkedList<char> dummy_list2;
+		dummy_list2 = dummy_list2;
+		dummy_list2.reverseLinkedList();
+		std::cout << "Reversing Linked List." << std::endl;
+		dummy_list2.printLinkedList();
+		std::cout << "--------------" << std::endl;
+	}
 
 	return 0;
 }
