30e03fa7b30e1603825e1058c8420f190b3c296e
Joey Yang <yangj30@mcmaster.ca>
2019-09-15 01:20:28 -0400

    adding same tree comparison solution

---
+++ leet_code/same_tree.cpp
@@ -0,0 +1,38 @@
+/*
+ * Runtime: 4 ms - Beats 58.85% of submissions
+ * Memory: 9.7 MB - Beats 100.00% of submissions
+ */
+
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+ * };
+ */
+class Solution {
+public:
+    bool isSameTree(TreeNode* p, TreeNode* q) {
+        //check tree structure
+        if (p == NULL && q == NULL)
+        {
+            return true;
+        }
+        else if (p == NULL || q == NULL)
+        {
+            return false;
+        }
+        //check node value
+        if (p->val != q-> val)
+        {
+            return false;
+        }
+        else
+        {
+            return (isSameTree(p->left, q->left) && isSameTree(p->right, q->right));
+        }
+    }
+};
+
