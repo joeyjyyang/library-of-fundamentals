811e52cd518fd6d79c39dd9f1d4da3c2a2a87da6
Joey Yang <yangj30@mcmaster.ca>
2020-09-26 03:48:27 -0400

    revise permutations

--- leet_code/medium/permutations.cpp
+++ leet_code/medium/permutations.cpp
@@ -1,52 +1,64 @@
 /*
  * Author: Joey Yang
  * Email: joeyyang.ai@gmail.com	
- * Runtime: 4 ms - Beats 87.67% of submissions
- * Memory: 8.1 MB - Beats 30.09% of submissions
+ * Runtime: 4 ms - Beats 87.71% of submissions
+ * Memory: 7.8 MB - Beats 51.56% of submissions
  * Data Structures:
  * Algorithms: Recursion, Backtracking, DFS
  * Other: Permutations
  */
 
+/*
+     [1]      [2]      [3]
+     / \      |  \      | \
+  [1,2][1,3][2,1][2,3][3,1][3,2]
+   /     \     \     \     \    \
+[1,2,3][1,3,2][2,1,3][2,3,1][3,1,2][3,2,1]
+*/
+
 class Solution {
 public:
     vector<vector<int>> permute(vector<int>& nums) {
         // Resulting permutations.
-        vector<vector<int>> permutations;
+        vector<vector<int>> answer;
         // Current permutation set.
-        vector<int> num_set;
+        vector<int> subset;
         // Beginning of recursive (tree) calls.
-        permuteBacktrack(permutations, num_set, nums);
-        
-        return permutations;
+        permuteBacktrack(answer, subset, nums);
+
+        return answer;
     }
-    
-    void permuteBacktrack(vector<vector<int>>& permutations, vector<int>& num_set, vector<int>& nums)
+
+    void permuteBacktrack(vector<vector<int>>& answer, vector<int>& subset, vector<int>& nums)
     {
-        // Push permutation set to answers array 
-        // if nums is empty, meaning there are no possible
-        // numbers to choose from.
-        if (nums.empty())
+        // Add permutation subset to answer if subset
+        // is the same size as nums; a complete permutation.
+        if (subset.size() == nums.size())
         {
-            permutations.push_back(num_set);
+            answer.push_back(subset);
         }
         else
         {
             for (int i = 0; i < nums.size(); i++)
             {
-                // Create new numbers with current number removed.
-                vector<int> new_nums = nums;
-                new_nums.erase(new_nums.begin() + i);
-                // Add removed number to current permutation set.
-                num_set.push_back(nums[i]);
-                // Recursive call, passing in new numbers,
-                // with 1 number removed (added to number set).
-                permuteBacktrack(permutations, num_set, new_nums);
-                // Remove number from current permutation set
-                // in the last position.
-                // Ready to add next number and perform recursion.
-                // Handled after returning from recursive call.
-                num_set.pop_back();
+                // Number already exists in subset, so skip process below.
+                if (find(subset.begin(), subset.end(), nums[i]) != subset.end())
+                {
+                    continue;
+                }
+                else
+                {
+                    // Add number to current permutation set.
+                    subset.push_back(nums[i]);
+                    // Recursive call, passing in new permutation set
+                    // with number added.
+                    permuteBacktrack(answer, subset, nums);
+                    // Remove number from current permutation set
+                    // in the last position.
+                    // Ready to add next number and perform recursion.
+                    // Handled after returning from recursive call.
+                    subset.pop_back();
+                }
             }
         }
     }
